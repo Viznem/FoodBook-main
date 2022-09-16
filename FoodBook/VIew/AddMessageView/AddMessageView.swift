8f51c9309adb0296b32d17313b009eeac0aaf6eb
Viznem <thinhtruong3344@gmail.com>
2022-09-16 20:23:46 +0700

    fix bug

--- FoodBook/VIew/AddMessageView/AddMessageView.swift
+++ FoodBook/VIew/AddMessageView/AddMessageView.swift
@@ -34,7 +34,6 @@ class AddMessageViewModel: ObservableObject {
                         if user.uid != FirebaseManager.shared.auth.currentUser?.uid {
                             self.users.append(.init(data: data))
                         }
-
                     })
                 }
         }
@@ -50,8 +49,21 @@ struct AddMessageView: View {
                ScrollView {
                    Text(vm.errorMessage)
                    
-                   ForEach(vm.users) { num in
-                       Text("New user")
+                   ForEach(vm.users) { user in
+                       HStack{
+                           WebImage(url: URL(string: user.profileImageUrl))
+                               .resizable()
+                               .frame(width: 50, height: 50)
+                               .clipped()
+                               .cornerRadius(50)
+                               .overlay(RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color(.label), lineWidth: 2)
+                               )
+                           Text(user.email)
+                           Spacer()
+                       }.padding(.horizontal)
+                       Divider()
+                           .padding(.vertical, 8)
                    }
                }.navigationTitle("New Message")
                    .toolbar {
